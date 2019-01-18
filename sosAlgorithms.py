2d6033f9f4ef37f0ad36999ff1d34fec54980ef9
yassine benmessahel <yassine.b@caramail.com>
2019-01-18 20:09:38 +0100

    Adding Selecting function S|O - Adding drawCell() - Adding lines draw when sos

--- sosAlgorithms.py
+++ sosAlgorithms.py
@@ -5,24 +5,32 @@ def newBoard(n):
     return(board)
 
 def possibleSquare(board, n, i, j):
-    if ((i >= 0 and j >= 0) and (i <= n and j <= n) and board[i][j] == 0):
-        return(True)
-    else:
-        return(False)
+        print(board[i][j], board)
+        if (board[i][j] == 0):
+                return(True)
+        else:
+                return(False)
 
 def updateScoreS(board, n, i, j, scores, player, lines):
     if (i == 0 and j == 0):
-        if (board[i][j + 1] == 2 and board[i][j + 2] == 1):
-            scores[player - 1] = scores[player - 1] + 1
-            lines.extend([i, j, i, j + 2])
-    elif (i == (n - 1) and j == (n - 1)):
-    elif (i == 0):
+        if (board[i][j] == 1 and board[i][j + 1] == 2 and board[i][j + 2] == 1):
+                scores[player - 1] = scores[player - 1] + 1
+                lines.extend([i, j, i, j + 2])
+                print("lines")
+        if (board[i][j] == 1 and board[i + 1][j] == 2 and board[i + 2][j] == 1):
+                scores[player - 1] = scores[player - 1] + 1
+                lines.extend([i, j, i, j + 2])
+    elif (i == (n - 1) == 1 and j == (n - 1)):
+            if (board[i][j] == 1 and board[i][j - 1] == 2 and board[i][j] == 2):
+                scores[player - 1] = scores[player - 1] + 1
+                lines.extend([i, j, i, j + 2])
+    '''elif (i == 0):
     elif (i == (n - 1)):
     elif (j == 0):
     elif (j == (n - 1)):
-    else:
+    else:'''
 
-def updateScoreO(board, n, i, j, scores, player, lines):
+'''def updateScoreO(board, n, i, j, scores, player, lines):
     if (i == 0 and j == 0):
         if (board[i][j + 1] == 2 and board[i][j + 2] == 1):
             scores[player - 1] = scores[player - 1] + 1
@@ -32,4 +40,4 @@ def updateScoreO(board, n, i, j, scores, player, lines):
     elif (i == (n - 1)):
     elif (j == 0):
     elif (j == (n - 1)):
-    else:
+    else:'''
--- sosGUI.py
+++ sosGUI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import pygame
 from pygame.locals import *
-'''import sosAlgorithms import *'''
+from sosAlgorithms import *
 
 def menu():
     GREY = (70, 70, 70)
@@ -25,10 +25,10 @@ def menu():
         for event in pygame.event.get():
             if event.type == MOUSEBUTTONDOWN:
                 if ((380 + 150) > mouse[0] > 380 and (240 + 50) > mouse[1] > 240):
-                    gameloop()
+                    gameloop(6, 70)
                     inProgress = False
                 if ((380 + 150) > mouse[0] > 380 and (310 + 50) > mouse[1] > 310):
-                    gameloop()
+                    gameloop(6, 70)
                     inProgress = False
             if event.type == QUIT:
                 inProgress = False
@@ -89,7 +89,16 @@ def drawBoardCell(mySurface, COLOR, x, y, size):
     pygame.draw.line(mySurface, COLOR, pos1, pos2)
     pygame.draw.line(mySurface, COLOR, pos2, pos3)
     pygame.draw.line(mySurface, COLOR, pos3, pos4)
-    pygame.draw.line(mySurface, COLOR, pos4, pos1)   
+    pygame.draw.line(mySurface, COLOR, pos4, pos1)
+    drawBoardLetter(mySurface, x, y, size)
+
+def drawBoardLetter(mySurface, x, y, size):
+    ORANGE = (255,140,0)
+    font = pygame.font.Font('font/Washington.ttf', 45)
+    text = font.render('S | O', True, ORANGE)
+    textRect = text.get_rect()
+    textRect.topleft = (x, (y + 15))
+    mySurface.blit(text, textRect)
 
 def displayScore(mySurface, n, scores):
     RED = (255, 0, 0)
@@ -120,14 +129,17 @@ def displayPlayer(mySurface, n, player):
 
 def drawCell(mySurface, board, i, j, player):
     letter = ''
-    x = (60 + 23 - (j + 5)) * (j + 1)
-    y = (60 + 15) * (i + 1)
+    x = (60 + 27 - (j + 5)) * (j + 1)
+    y = (60 + 13) * (i + 1)
     RED = (255, 0, 0)
     BLUE = (0, 0, 255)
-    font = pygame.font.Font('font/Washington.ttf', 48)
+    font = pygame.font.Font('font/Washington.ttf', 55)
+    clearCell(mySurface, board, i, j)
     if (board[i][j] == 1):
+        print("draw s")
         letter = 'S'
-    else:
+    elif(board[i][j] == 2):
+        print("draw o")
         letter = 'O'
     if (player == 1):
         text = font.render(letter, True, BLUE)
@@ -135,31 +147,106 @@ def drawCell(mySurface, board, i, j, player):
         textRect.topleft = (x, y)
         mySurface.blit(text, textRect)
     else:
-        text = font.render(letter, True, BLUE)
+        text = font.render(letter, True, RED)
         textRect = text.get_rect()
         textRect.topleft = (x, y)
         mySurface.blit(text, textRect)
 
-def gameloop():
-    board = [[1] * 6 for i in range(6)]
+def clearCell(mySurface, board, i, j):
+    GREY = (70, 70, 70)
+    x = 61 * (j + 1) + (j * 9)
+    y = 61 * (i + 1) + (i * 9)
+    rect = (x, y, 69, 69)
+    pygame.draw.rect(mySurface, GREY, rect)
+
+def drawLines(mySurface, lines, player):
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    
+    if (lines):
+        x = 60 * (lines[1] + 1)
+        y = 60 * (lines[0] + 1) + 35
+        x1 = 70 * (lines[3] + 1) + 20
+        y1 = 60 * (lines[2] + 1) + 35
+        pos1 = (x, y)
+        pos2 = (x1, y1)
+        if (player == 1):
+            pygame.draw.line(mySurface, RED, pos1, pos2)
+            print("drawing", pos1, pos2)
+        elif (player == 2):
+            pygame.draw.line(mySurface, BLUE, pos1, pos2)
+
+def selectSquare(mySurface, board, n, size):
+    i = 0
+    j = 0
+    x = 70
+    y = 70
+    half = size/2
+    position = [-1, -1]
+    mouse = pygame.mouse.get_pos() 
+
+    while (j != n):
+        while (i != n):
+            if ((60 + (x - half)) > mouse[0] > (60 + (size * i) - half) and (60 + y) > mouse[1] > (60 + (size * j))
+            and possibleSquare(board, n, j, i) == True):
+                position[0] = i
+                position[1] = j
+                print("s", i, j, x, y, mouse[0], mouse[1])
+                board[j][i] = 1
+                return (position)
+            elif ((60 + x) > mouse[0] > (60 + (size * i) + half) and (60 + y) > mouse[1] > (60 + (size * j))
+            and possibleSquare(board, n, j, i) == True):
+                position[0] = i
+                position[1] = j
+                print("o", i, j)
+                board[j][i] = 2
+                return (position)
+            x = x + size
+            i = i + 1
+        i = 0
+        x = 60 
+        j = j + 1
+        y = y + size
+    return (position)
+
+
+def gameloop(n, size):
+    board = [[0] * 6 for i in range(6)]
+
     GREY = (70, 70, 70)
     pygame.init()
     mySurface = pygame.display.set_mode((900, 600))
     pygame.display.set_caption('SOS')
     inProgress = True
+    player = 1
+    length = 0
+    scores = [0, 0]
 
+    mySurface.fill(GREY)
+    drawBoard(mySurface, n)
     while inProgress:
+        position = [-1, -1]
+        lines = []
+        displayScore(mySurface, 6, scores)
         for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN:
+                position = selectSquare(mySurface, board, n, size)
+                print(board)
             if event.type == QUIT:
                 inProgress = False
-        mySurface.fill(GREY)
-        drawBoard(mySurface, 6)
-        drawCell(mySurface, board, 0, 2, 1)
+        if (position[0] != -1):
+            drawCell(mySurface, board, position[1], position[0], player)
+            updateScoreS(board, n, position[1], position[0], scores, player, lines)
+            drawLines(mySurface, lines , player)
+            displayScore(mySurface, 6, scores)
+            if (player == 2):
+                player = 1
+            else:
+                player = player + 1
         pygame.display.update()
     pygame.quit()
 
-menu()  
+menu()
 
-'''def drawLines(mySurface,lines,player):
-
-def displayWinner(mySurface,n,scores):'''
+'''def displayWinner(mySurface,n,scores):
+        selectSquare(mySurface, board, n)'''
